b0e73d5d239d3590a96c0f30906438ead764e1ea
therealcraiggazimbi <craiggazimbi@gmail.com>
2024-01-10 14:12:10 +0200

    day-1-setup-complete

--- DEVember-test/src/app/(days)/day1/index.tsx
+++ DEVember-test/src/app/(days)/day1/index.tsx
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Stack } from "expo-router";
 
 const index = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Stack.Screen options={{ title: "Day 1" }} />
+      <Text style={{ fontFamily: "AmaticBold", fontSize: 100 }}>
+        Day 1 Details Screen
+      </Text>
     </View>
   );
 };
--- DEVember-test/src/app/index.tsx
+++ DEVember-test/src/app/index.tsx
@@ -1,36 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, FlatList, ActivityIndicator } from "react-native";
-import DayListItem from "../components/core/DayListItem";
-
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
-import {
-  AmaticSC_400Regular,
-  AmaticSC_700Bold,
-} from "@expo-google-fonts/amatic-sc";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
+import { StyleSheet, View, FlatList } from "react-native";
+import DayListItem from "@components/core/DayListItem";
 
 export default function HomeScreen() {
   const days = [...new Array(24)].map((val, index) => index + 1);
-
-  const [fontsLoaded, fontError] = useFonts({
-    Inter: Inter_900Black,
-    Amatic: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold,
-  });
-
-  useEffect(() => {
-    if (fontsLoaded || fontError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
 
   return (
     <View style={styles.container}>
